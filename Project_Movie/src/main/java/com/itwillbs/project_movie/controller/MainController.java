7e7a70a469ad6c011d9d35c9cb9529679e8ce2a9
정영훈 <JYH>
2025-01-06 18:53:33 +0900

    변동 없음

--- Project_Movie/src/main/java/com/itwillbs/project_movie/controller/MainController.java
+++ Project_Movie/src/main/java/com/itwillbs/project_movie/controller/MainController.java
@@ -1,10 +1,8 @@
 package com.itwillbs.project_movie.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.itwillbs.project_movie.service.MemberService;
 
 @Controller
 public class MainController {
